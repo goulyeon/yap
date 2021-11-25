2d2b88740a93c716d6a28e992415dbafa2f2923b
goulyeon <52442755+goulyeon@users.noreply.github.com>
2021-11-25 15:08:09 +0900

    1125

--- js/main.js
+++ js/main.js
@@ -87,7 +87,7 @@ $(document).ready(function(){
 
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 4,
-        spaceBetween: 20,
+        spaceBetween: 30,
         freeMode: true,
         pagination: {
           el: ".swiper-pagination",
